e81985143733e56f2a960e04919ee5655dc01e8a
Amr <amrabdelfattah321@gmail.com>
2024-04-13 21:13:23 +0200

    Added CLI mode

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include <string>
 using namespace std;
 
+class CLI {
+    private:
+        string input;
+
+    public:
+        void printName()
+        {
+            cout << "This is a test." << endl;
+        }
+};
+
 int main()
 {
     string interface;
@@ -29,12 +40,29 @@ int main()
     // CLI CODE
     if (interface[0] == '1')
     {
-        ;
+        string input;
+
+        while (1)
+        {
+            cout << "$ ";
+            getline(cin, input);
+            
+            if (input.substr(0, 10) == "fetch duhr" && input.length() == 10)
+            {
+                cout << "working" << endl;
+            }
+            else if ((input.substr(0, 4) == "quit" && input.length() == 4) || (input[0] == '0' && input.length() == 1) || (input[0] == 'q' && input.length() == 1))
+            {
+                cout << "Exiting CLI mode." << endl;
+                break;
+            }
+        }
     }
 
     // GUI CODE
     if (interface[0] == '2')
     {
+        // Enter GUI mode.
         cout << " *** NOT OPERATIONAL AT THE MOMENT *** " << endl;
     }
 
