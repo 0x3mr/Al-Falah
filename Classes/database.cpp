45794c113426cbdef36912db323c1faa2e43558d
OxHazem <omarhazemhassan@gmail.com>
2026-03-19 08:00:26 +0200

    Adding the creation of the new table of the DB

--- Classes/database.cpp
+++ Classes/database.cpp
@@ -57,6 +57,21 @@ void Database::initialize() {
             prayer_name TEXT NOT NULL,
             adhan_time TIME NOT NULL
         );
+        
+        CREATE TABLE prayer_tracking (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            prayer_name TEXT NOT NULL,
+            date TEXT NOT NULL,
+
+            status TEXT DEFAULT 'missed', 
+            -- 'prayed', 'missed', 'late'
+
+            prayed_at TEXT, 
+            -- actual time user marked it done
+
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        );
 
 
 
