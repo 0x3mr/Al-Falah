3b9c4bfac98169cd585986b440c0780a79fd5524
OxHazem <omarhazemhassan@gmail.com>
2026-03-20 17:05:54 +0200

    Adding the prayer_timesql

--- db/prayer_times.sql
+++ db/prayer_times.sql
@@ -1,9 +1,5 @@
 CREATE TABLE prayer_times (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    prayer_name TEXT NOT NULL,
+    prayer_name TEXT NOT NULL UNIQUE,
     adhan_time TEXT NOT NULL,
-    date TEXT NOT NULL,
-
-    FOREIGN KEY(user_id) REFERENCES users(id)
 );
