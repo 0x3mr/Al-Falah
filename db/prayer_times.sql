b8dcfbc4eec46397c69682f159ec7769e68d68d3
OxHazem <omarhazemhassan@gmail.com>
2026-04-24 17:49:31 +0300

    fixing the prayer_times DB table

--- db/prayer_times.sql
+++ db/prayer_times.sql
@@ -1,5 +1,7 @@
 CREATE TABLE prayer_times (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    prayer_name TEXT NOT NULL UNIQUE,
+    user_id INTEGER NOT NULL,
+    prayer_name TEXT CHECK(prayer_name IN ('Fajr','Dhuhr','Asr','Maghrib','Isha')),
     adhan_time TEXT NOT NULL,
+    PRIMARY KEY (user_id, prayer_name),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
