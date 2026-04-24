7d915862765cc1d012857907dc709303f0c3a8fd
OxHazem <omarhazemhassan@gmail.com>
2026-04-24 17:49:03 +0300

    fixing the settings DB table

--- db/settings.sql
+++ db/settings.sql
@@ -1,6 +1,5 @@
 CREATE TABLE user_settings (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER PRIMARY KEY,
     adhan_enabled BOOLEAN DEFAULT 1,
     adhan_sound TEXT DEFAULT 'default.mp3',
     FOREIGN KEY (user_id) REFERENCES users(id)
